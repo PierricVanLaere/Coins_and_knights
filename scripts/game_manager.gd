7e94bf1c5e417ac19c2730dc2a7f6add19df021b
Pierric VanLaere <pierric.van-laere@etu.univ-nantes.fr>
2024-12-21 20:48:10 +0100

    Mise en place de niveaux, et de fin de partie

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -1,8 +1,22 @@
 extends Node
 
+@onready var timer = $Timer
 @onready var coins_counter = $"../Player/coins_counter"
 var score = 0
+var objectif = 10
+
 
 func add_point():
 	score += 1
 	coins_counter.text = str(score)
+	
+func _on_timer_timeout() -> void:
+	Engine.time_scale = 1
+	get_tree().change_scene_to_file("res://scenes/selection_levels.tscn")
+	
+func _process(delta: float) -> void:
+	if score == objectif:
+		coins_counter.text = str("Gagné !")
+		score = 0
+		Engine.time_scale = 0.5
+		timer.start()
